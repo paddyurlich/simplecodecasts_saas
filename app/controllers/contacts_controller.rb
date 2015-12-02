375adf4442be8965f69578371492092af2d7c54a
Paddy Urlich <paddyurlich@gmail.com>
2015-12-02 09:21:08 +0000

    temp removal of contact form action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,16 +2,16 @@ class ContactsController < ApplicationController
     def new
         @contact = Contact.new
     end
-    
+  
     def create
         @contact = Contact.new(contact_params)
         
         if @contact.save
-            name = params[:contact][:name]
-            email = params[:contact][:email]
-            body = params[:contact][:comments]
+        #    name = params[:contact][:name]
+        #    email = params[:contact][:email]
+        #    body = params[:contact][:comments]
             
-            ContactMailer.contact_email(name,email,body).deliver
+        #    ContactMailer.contact_email(name,email,body).deliver
             
             flash[:success] = "Message Sent."
             redirect_to new_contact_path
